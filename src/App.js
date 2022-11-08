cb9bcb13ad4522bbc8dbaf5af3edf71aedcbb29d
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-08 17:14:51 +0000

    added main css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from "react";
+import "./main.css";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return <div>App</div>;
+};
 
-export default App
-
+export default App;
