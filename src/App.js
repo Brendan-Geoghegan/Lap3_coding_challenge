e3fc263520c524045f1c4bccdd0b6ba72b6d43eb
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-08 17:03:15 +0000

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts'
+import { Home, NotFound, User } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="user" element={<User />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
 
