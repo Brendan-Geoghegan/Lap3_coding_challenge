5da3a12f8f16b47cc6538fa8eaa451abcf4769c0
Brendan-Geoghegan <brendangeoghegan@outlook.com>
2022-11-09 11:44:59 +0000

    file loader added and header removed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import { UserForm } from '../../components'
+import image from './github.png'
 
 const Home = () => {
   return (
     <div className='main-container'>
     <h1>GitHub Repo Tracker</h1>
-    <img src='./github.png' alt='GitHub logo' />
+    <img src={image} alt='GitHub logo' />
     <UserForm />
     </div>
   )
