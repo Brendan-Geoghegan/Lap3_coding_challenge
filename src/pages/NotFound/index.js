f090ea0afe5f388eafbf717686eafbe4cb3bcc69
aimyv <av533@bath.ac.uk>
2022-11-09 16:44:58 +0000

    added home button to not found

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 
 const NotFound = () => {
   const location = useLocation()
   return (
     <>
     <h3>Error 404: Page not found with pathname {location.pathname}</h3>
+    <button id='home'><Link to='/'>Home</Link></button>
     </>
   )
 }
