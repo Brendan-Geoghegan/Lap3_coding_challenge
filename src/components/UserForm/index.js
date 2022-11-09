70213ff5ab4bbcf79cf61d58ce7e644710d4d79e
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-09 13:49:08 +0000

    updated homepage transition

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,29 +1,34 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux';
-import { getUserResult } from '../../actions';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getUserResult } from "../../actions";
+import { useNavigate } from "react-router-dom";
 
 const UserForm = () => {
-  const [ formData, setFormData ] = useState("")
+	const [formData, setFormData] = useState("");
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(getUserResult(formData))
-    navigate("/user")
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch(getUserResult(formData));
+		navigate("/user");
+	};
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='username'>Enter username:</label>
-      <br/>
-      <input type="text" placeholder="GitHub Username" id='username' onChange={(e) => setFormData(e.target.value)} />
-      <br/>
-      <input type="submit" value="Submit" />
-    </form>
-  )
-}
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="username">Enter username:</label>
+			<br />
+			<input
+				type="text"
+				placeholder="GitHub Username"
+				id="username"
+				onChange={(e) => setFormData(e.target.value)}
+			/>
+			<br />
+			<input type="submit" value="Submit" />
+		</form>
+	);
+};
 
-export default UserForm
+export default UserForm;
