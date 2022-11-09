8712a614cda7477c3c19ef0888c62cfd51ad9f1b
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-09 13:22:07 +0000

    animated routes again

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -11,7 +11,7 @@ const User = () => {
 		<motion.div
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1, transition: { duration: 1 } }}
-			exit={{ opacity: 0, transition: { duration: 0.1 } }}
+			exit={{ opacity: 0 }}
 		>
 			<BackButton />
 			<br />
